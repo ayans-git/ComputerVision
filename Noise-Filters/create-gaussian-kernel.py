52a29308ddcc4848f75e21ec7f79125a44c425f9
ayans-git <57875525+ayans-git@users.noreply.github.com>
2020-06-21 17:32:39 -0400

    Delete create-gaussian-kernel.py

--- Noise-Filters/create-gaussian-kernel.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from scipy.signal import gaussian
-
-def create_gaussian_kernel(kernel_size = 3):
-    kernel = gaussian(kernel_size, kernel_size/3).reshape(kernel_size, 1)
-    kernel = np.dot(kernel, kernel.transpose())
-    kernel /= np.sum(kernel)
-    return kernel
